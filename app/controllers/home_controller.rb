05baca68f3b6fc695c884f1b7e018d5a083fd216
flighting <2212044830@qq.com>
2018-03-09 11:37:31 +0800

    首页不限制精华

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @excellent_topics = Topic.excellent.recent.fields_for_list.limit(20).to_a
+    # @excellent_topics = Topic.excellent.recent.fields_for_list.limit(20).to_a
+    @excellent_topics = Topic.recent.fields_for_list.limit(20).to_a
   end
 
   def uploads
